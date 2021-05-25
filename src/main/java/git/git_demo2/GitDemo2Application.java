ba17b1358edb464680b8bf8f5fe4df126c278b42
Wky <2248338536@qq.com>
2021-05-25 09:59:30 +0800

    second commit!

---
+++ src/main/java/git/git_demo2/GitDemo2Application.java
@@ -0,0 +1,16 @@
+package git.git_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class GitDemo2Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(GitDemo2Application.class, args);
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+
+}
